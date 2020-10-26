93ba9703a2de4823846b3e5e097fb1d92fa0cd3c
emmanordangaard@gmail.com <emmanordangaard@gmail.com>
2020-10-26 18:23:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inl.uppgift.version1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool socialSecurityValid = false;
+
+            while (socialSecurityValid != true)
+            {
+                Console.WriteLine("Write your social security number with 12 numbers: ");
+                string userInput = Console.ReadLine();
+                int numCharacter = userInput.Length;
+
+                if (numCharacter == 12)
+                {
+                    socialSecurityValid = true;
+                }
+            }
+
+        }
+    }
+}
